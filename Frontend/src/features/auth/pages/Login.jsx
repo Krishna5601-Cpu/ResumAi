ccc0347799d827c0c25f74b95e6a06a6e9d3fc9e
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-06-29 23:08:22 +0530

    feat(frontend): Styled ui for login & register page

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import "../auth.form.scss";
+import { useNavigate, Link } from "react-router";
 
 const Login = () => {
-  return <div>Login</div>;
+  useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email address"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <button className="button primary-button">Login</button>
+        </form>
+
+        <p>
+          Don't have an account <Link to={"/register"}>Register</Link>
+        </p>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
