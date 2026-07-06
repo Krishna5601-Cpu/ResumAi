d469cd811ee8f4352691b399fa776b463c618c86
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-07-06 22:30:43 +0530

    feat: CORS setup for frontend - backend communication

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRouter = require("./routes/auth.routes");
+const cors = require("cors");
 
-const app = express(); // Create server instance 
+const app = express();
 
 app.use(express.json);
 app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 /* using all the routes here - */
 app.use("/api/auth", authRouter);
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, Link } from "react-router";
+import { useAuth } from "../hooks/useAuth";
 
 const Register = () => {
-  useNavigate();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = (e) => {
+  const { loading, handleRegister } = useAuth();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    await handleRegister({ username, email, password });
+    navigate("/");
   };
+
+  if (loading) {
+    return <main>Loading.....</main>;
+  }
 
   return (
     <main>
@@ -17,6 +30,9 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="email">Username</label>
             <input
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
               type="text"
               id="username"
               name="username"
@@ -26,6 +42,9 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="email">Email</label>
             <input
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
               type="email"
               id="email"
               name="email"
@@ -35,6 +54,9 @@ const Register = () => {
           <div className="input-group">
             <label htmlFor="password">Password</label>
             <input
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
               type="password"
               id="password"
               name="password"
