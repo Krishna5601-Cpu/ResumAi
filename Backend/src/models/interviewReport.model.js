ce0b124c1add9cb028d8a9fca7d8a2812ea09d36
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-07-08 22:16:49 +0530

    feat(database): interview, report db model

---
+++ Backend/src/models/interviewReport.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+
+/**
+ * - job description schema
+ * - resume text
+ * - self description
+ * 
+ * technical questions : [{
+ * 
+ * question: "",
+ * intention: "",
+ * answer: ""
+ * 
+ * }]
+ * 
+ * behavioural questions : [{
+ * 
+ * question: "",
+ * intention: "",
+ * answer: ""
+ * 
+ * }]
+ * 
+ * skill gaps : [{
+ * skills: "",
+ * sevirity: {
+ * type: string
+ * enum: ["Low", "Medium", "High"]
+ * }
+ * }]
+ * preparation plan 
+ */
+
+const interviewReportModel = new mongoose.Schema(
+
+)
+
+
+
+module.exports = interviewReportModel;
