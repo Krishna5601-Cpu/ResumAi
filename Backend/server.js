2835a28d12b0206717798fc5c5e8d658d2754984
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-07-10 01:58:55 +0530

    feat(ai): interview report zod schema & ai response structure

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,8 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/config/database");
-const invokeGeminiAi = require("./src/services/ai.service");
 
 connectToDB();
-invokeGeminiAi();
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,21 +1,47 @@
-const { GoogleGenAI } = require("@google/genai");
-const { model } = require("mongoose");
+const { GoogleGenAI, Behavior } = require("@google/genai");
+const { model, default: mongoose } = require("mongoose");
+const { z } = require("zod");
+const { zodToJsonSchema } = require("zod-to-json-schema");
+const { _enum } = require("zod/v4/core");
 
 const ai = new GoogleGenAI({
-  apiKey: process.env.GOOGLE_GENAI_API_KEY
+  apiKey: process.env.GOOGLE_GENAI_API_KEY,
 });
 
 
-async function invokeGeminiAi(params) {
+const interviewReportSchema = z.object({
+  matchScore: z.number().description("a score between 0 and 100 indicating how well the candidate's profile matches the job"),
 
 
-  const responese = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "Hello Gemini, what is interview?",
-  })
+  technicalQuestions: z.array(z.object({
+    question: z.string().description("the interview questions that can be asked in the interview "),
+    intention: z.string().description("the intention behind the question asked in interview "),
+    answer: z.string().description("how to answer this question, what points to cover, how to approach"),
+  })).description("the technical questions that can be asked in the interview with their intention"),
+})
 
-  console.log(responese.text);
+behavioralQuestions: z.object({
+  technicalQuestions: z.array(z.object({
+    question: z.string().description("the interview questions that can be asked in the interview "),
+    intention: z.string().description("the intention behind the question asked in interview "),
+    answer: z.string().description("how to answer this question, what points to cover, how to approach"),
+  })).description("Behavioural questions that can be asked in the interview along with their intention"),
+})
+
+skillGaps: z.object({
+  skill: z.string().description("The skills that candidate is lacking "),
+  severity: z.enum("low", "medium", "high")
+}).description("the list of skills gap in the candidate's profile along with their inttention"),
+  preparationPlan : z.array(z.object({
+    days: z.number().description("the day number n the preparation plan, starting from day 1."),
+    focus: z.string().description('the main focus of this day in the preparation plan e.g data strucutre, system design, mock interviews '),
+    tasks: z.array(z.string).description("a daywise preparation plan for the candidate to follow in order to crack the job")
+  }))
+
+async function generateInterviewReport({ resume, selfDescription, jobDescription }) {
 
 }
+
+
 
 module.exports = invokeGeminiAi;
