8ea321a589fcb9457033923f3165ff0f8d0ad587
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-07-14 19:37:44 +0530

    home page design and styling

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
+import Home from "./features/interview/pages/Home";
 import Protected from "./features/auth/components/Protected";
 
 export const router = createBrowserRouter([
@@ -16,7 +17,7 @@ export const router = createBrowserRouter([
     path: "/",
     element: (
       <Protected>
-        <h1>Home</h1>
+        <Home />
       </Protected>
     ),
   },
