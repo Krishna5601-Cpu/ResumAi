6daf2cdcbd64b7732872142b83bfa6dd18ea0efb
Krishna5601-Cpu <chauhankrishna2724@gmail.com>
2026-07-20 17:54:21 +0530

    feat(frontend): custom useInterview hook

--- Frontend/src/features/interview/interview.context.jsx
+++ Frontend/src/features/interview/interview.context.jsx
@@ -1,20 +1,16 @@
 import { createContext, useState } from "react";
 
-export const interviewContext = createContext();
 
-export const interviewProvider = ({ children }) => {
+export const InterviewContext = createContext()
 
-  const [loading, setLoading] = useState(false);
-  const [report, setReport] = useState(null);
-  const [reports, setReports] = useState([]);
-
+export const InterviewProvider = ({ children }) => {
+  const [loading, setLoading] = useState(false)
+  const [report, setReport] = useState(null)
+  const [reports, setReports] = useState([])
 
   return (
-    <interviewContext.Provider value={loading, setLoading, report, setLoading, report, setReports}>
+    <InterviewContext.Provider value={{ loading, setLoading, report, setReport, reports, setReports }}>
       {children}
-    </interviewContext.Provider>
+    </InterviewContext.Provider>
   )
-
-
-
 }
